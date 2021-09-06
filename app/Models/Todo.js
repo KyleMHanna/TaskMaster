33a1e8b80ef9f677823e4696dfb57d7a63d7ede1
Kyle Hanna <Hanna.Kyle@live.com>
2021-09-05 22:12:11 -0600

    Almost done.

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -10,14 +10,14 @@ export class Todo{
   }
   get Template(){
     return /*html*/ `
-      <div class="row ">
+      <div class="row shadow-lg rounded ">
       <div class="todos">
       </div>
-      <div class="col">
+      <div class="col shadow-lg rounded p-3 text-light">
     <p  class="${this.taskId}" onclick="app.todosController.completeTodo('${this.id}')">${this.title} <input type="checkbox" value="" ${this.checked ? "checked" : ""} >  </p>
     
     </div>
-    <div class="col-1">
+    <div class="col-1 shadow-lg rounded p-3">
     <button class="text-danger close" onclick="app.todosController.deleteTodo('${this.id}')"><i class="mdi mdi-delete"></i></button>
     </div>
     </div>
